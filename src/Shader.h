0067803a7d4b1ef894a5a54b31d0e266a3fdb62d
myleshansen <myleshunter@hotmail.com>
2025-04-18 12:08:46 -0700

    Remove BufferLayout

--- src/Shader.h
+++ src/Shader.h
@@ -26,6 +26,7 @@ public:
     void Unbind() const;
 
     // Set uniforms
+    void SetUniform1f(const std::string& name, float v0);
     void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
 private:
     ShaderProgramSource ParseShader(const std::string& filepath);
